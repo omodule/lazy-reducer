a87e4617b4d37d4db073d1c287e2a3f325e9ed22
wuchangming <wuchangmingnice@163.com>
2017-12-07 20:39:25 +0800

    add withLazyReducer test

--- src/withLazyReducer.js
+++ src/withLazyReducer.js
@@ -1,7 +1,12 @@
-import { Component, createElement } from 'react';
-import storeShape from './storeShape';
-import isPlainObject from 'lodash/isPlainObject';
-import { combineReducers } from 'redux';
+import { Component, createElement } from 'react'
+import storeShape from './storeShape'
+import isPlainObject from 'lodash/isPlainObject'
+import { combineReducers } from 'redux'
+import React from 'react'
+
+function getDisplayName(WrappedComponent) {
+    return WrappedComponent.displayName || WrappedComponent.name || 'Component'
+}
 
 /**
  * lazyReducer structure:
@@ -16,40 +21,40 @@ import { combineReducers } from 'redux';
  * @return {[type]}                    [description]
  */
 const withLazyReducer = (lazyReducer, storeKey = 'store') => WrappedComponent => {
-    const displayName = `withLazyReducer(${WrappedComponent.displayName})`;
+    const displayName = `withLazyReducer(${getDisplayName(WrappedComponent)})`
 
     class ComponentWithLazyReducer extends Component {
         constructor(props, context) {
-            super(props);
-            const store = context[storeKey];
+            super(props)
+            const store = context[storeKey]
             if (!store) {
                 throw new Error(
                     `Could not find "${storeKey}" in either the context or props of ` +
                         `"${displayName}". Either wrap the root component in a <Provider>, ` +
                         `or explicitly pass "${storeKey}" as a prop to "${displayName}".`
-                );
+                )
             }
             if (isPlainObject(lazyReducer)) {
-                store.addLazyReducers(lazyReducer);
+                store.addLazyReducers(lazyReducer)
                 store.replaceReducer(
                     combineReducers({
                         ...store.getSyncReducers(),
                         ...store.getLazyReducers()
                     })
-                );
+                )
             } else {
-                throw new Error('You must pass a `planObject` to the function `withLazyReducer`');
+                throw new Error('You must pass a `planObject` to the function `withLazyReducer`')
             }
         }
         render() {
-            return createElement(WrappedComponent, this.props);
+            return <WrappedComponent {...this.props} />
         }
     }
     ComponentWithLazyReducer.contextTypes = {
         [storeKey]: storeShape
-    };
-    ComponentWithLazyReducer.displayName = displayName;
-    return ComponentWithLazyReducer;
-};
+    }
+    ComponentWithLazyReducer.displayName = displayName
+    return ComponentWithLazyReducer
+}
 
-export default withLazyReducer;
+export default withLazyReducer
---
+++ test/withLazyReducer.spec.js
@@ -0,0 +1,73 @@
+import { withLazyReducer, lazyReducerEnhancer } from '../lib'
+import { createStore, combineReducers } from 'redux'
+import renderer from 'react-test-renderer'
+import React, { Component } from 'react'
+import { connect, Provider } from 'react-redux'
+
+@connect(state => {
+    return {
+        store: state
+    }
+})
+@withLazyReducer({
+    nameX: function(state = null, action) {
+        return state
+    }
+})
+class InnerComponent extends Component {
+    render() {
+        return <div>InnerComponent {JSON.stringify(this.props)}</div>
+    }
+}
+
+@connect(state => {
+    return {
+        store: state
+    }
+})
+class DelayContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+        setTimeout(() => {
+            this.setState({
+                show: true
+            })
+        }, 200)
+    }
+
+    render() {
+        if (this.state.show) {
+            return <InnerComponent />
+        } else {
+            return <div>DelayContainer {JSON.stringify(this.props)}</div>
+        }
+    }
+}
+
+function waitFor(delay) {
+    return new Promise(resolve => {
+        setTimeout(resolve, delay)
+    })
+}
+
+test('withLazyReducer', async () => {
+    const syncReducers = {
+        a: (state = 'a', action) => {
+            return state
+        },
+        b: (state = 'x', action) => {
+            return state
+        }
+    }
+    const store = createStore(combineReducers(syncReducers), {}, lazyReducerEnhancer(syncReducers))
+    const App = (
+        <Provider store={store}>
+            <DelayContainer />
+        </Provider>
+    )
+    const app = renderer.create(App)
+    expect(app.toJSON()).toMatchSnapshot()
+    await waitFor(300)
+    expect(app.toJSON()).toMatchSnapshot()
+})
